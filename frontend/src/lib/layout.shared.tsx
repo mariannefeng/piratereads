54717fd04c0b1867a2bf610269bae19ad879e15c
Marianne Feng <feng.marianne@gmail.com>
2026-03-07 15:22:31 -0500

    add logo to sidebar, change theme

--- frontend/src/lib/layout.shared.tsx
+++ frontend/src/lib/layout.shared.tsx
@@ -1,4 +1,6 @@
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import Logo from "@/app/icon.svg";
+import Image from "next/image";
 
 // fill this with your actual GitHub info, for example:
 export const gitConfig = {
@@ -10,7 +12,18 @@ export const gitConfig = {
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: "piratereads",
+      title: (
+        <>
+          <Image
+            src={Logo}
+            alt="piratereads"
+            width={16}
+            height={16}
+            className="dark:invert"
+          />
+          piratereads
+        </>
+      ),
     },
     githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`,
   };
